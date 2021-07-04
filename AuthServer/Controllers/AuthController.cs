56c47e9a46db4967aeeb3f67867d0ba05d582e4b
Jericho Siahaya <jericho.siahaya@gmail.com>
2021-07-04 22:35:21 +0700

    Add project files.

---
+++ AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,85 @@
+﻿using AuthServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+
+namespace AuthServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        readonly IConfiguration _configuration;
+        private IOptions<Audience> _settings;
+        //private IConfiguration<Audience> s;
+        /* TO DO -->> DONE
+         * add DI to read secret value from appsettings.json
+         */
+
+        public AuthController(IOptions<Audience> settings, IConfiguration configuration)
+        {
+            this._settings = settings;
+            this._configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string name, string pwd)
+        {
+            if (name == "daftpunk" && pwd == "aroundtheworld")
+            {
+                //var s = "Y2F0Y2hlciUyMHdvbmclMjBsb3ZlJTIwLm5ldA==";
+                var now = DateTime.UtcNow;
+
+                var claims = new Claim[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, name),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iat, now.ToUniversalTime().ToString(), ClaimValueTypes.Integer64)
+                };
+                var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration.GetValue<string>("Audience:Secret"))); // pass the value here
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = signingKey,
+                    ValidateIssuer = true,
+                    ValidIssuer = _settings.Value.Iss,
+                    ValidateAudience = true,
+                    ValidAudience = _settings.Value.Aud,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    RequireExpirationTime = true,
+                };
+                var jwt = new JwtSecurityToken(
+                    issuer: _settings.Value.Iss,
+                    audience: _settings.Value.Aud,
+                    claims: claims,
+                    notBefore: now,
+                    expires: now.Add(TimeSpan.FromMinutes(15)),
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+                );
+                var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+                var responseJson = new
+                {
+                    access_token = encodedJwt,
+                    expires_in = (int)TimeSpan.FromMinutes(15).TotalSeconds
+                };
+
+                return Ok(responseJson);
+            }
+            else
+            {
+                return Ok("Username/Password salah!");
+            }
+        }
+
+     }
+}
